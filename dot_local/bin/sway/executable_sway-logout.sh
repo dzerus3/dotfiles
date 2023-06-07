b1416584647a0ab4a11fab9000b7140242854755
Dellon Zerus <dellonzerus@gmail.com>
2023-06-06 18:59:31 -0700

    Added lock option to logout screen

--- dot_local/bin/sway/executable_sway-logout.sh
+++ dot_local/bin/sway/executable_sway-logout.sh
@@ -2,23 +2,24 @@
 # See also https://github.com/jluttine/rofi-power-menu/blob/master/rofi-power-menu
 
 options=(
+    "✱  Lock"
     "⍈  Logout"
-    "⏻  Shutdown"
-    "⏼  Reboot"
     "⏾  Suspend"
+    "⏻  Shutdown"
+    "↻  Reboot"
     "⨯  Cancel"
 )
 
 selection=$(
     printf '%s\n' "${options[@]}" | \
-    fuzzel --dmenu --lines=5 --width=13
+    fuzzel --dmenu --lines=6 --width=13
 )
 
 # Note: User needs permission in /etc/doas.conf
 case "$selection" in
-    "⍈  Logout")   swaymsg exit         ;;
+    "✱  Lock")     swaylock --daemonize -i $HOME/pics/screensavers/screensaver.jpg ;;
+    "⍈  Logout")   swaymsg exit ;;
+    "⏾  Suspend")  swaylock --daemonize -i $HOME/pics/screensavers/screensaver.jpg && doas -n s2ram          ;;
     "⏻  Shutdown") doas -n shutdown -h now ;;
-    "⏼  Reboot")   doas -n shutdown -r now ;;
-    "⏾  Suspend")  swaylock --daemonize -i /usr/share/backgrounds/screensaver.jpg && doas -n s2ram          ;;
+    "↻  Reboot")   doas -n shutdown -r now ;;
 esac
-
