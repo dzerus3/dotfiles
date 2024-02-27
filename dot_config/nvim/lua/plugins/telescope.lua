8ec3b45a076f973c04ddc18cd762cab38fe5e160
Dellon Zerus <dellonzerus@gmail.com>
2024-02-27 14:39:25 -0800

    (Re)added telescope to nvim

---
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,21 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+        { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find Files" },
+        { "<leader>ft", "<cmd>Telescope live_grep<CR>", desc = "Find a string" },
+        { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Find buffers" },
+        { "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = "Help" },
+        { "<leader>fk", "<cmd>Telescope keymaps<CR>", desc = "Find keymaps" },
+    },
+    opts = {
+        defaults = {
+            file_ignore_patterns = { "node%_modules/*", ".yarn/*", ".git/*", ".idea/*" },
+        },
+        pickers = {
+            find_files = {
+                hidden = true,
+            },
+        },
+    },
+}
